7d341aa3e88808afc4a40de673805adffd2da359
Punnoose Punnen <punnoose88@gmail.com>
2023-11-09 14:35:13 +0530

    shad cn ui, theme switcher

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { UserButton } from '@clerk/nextjs';
+import Logo from '@/components/Logo';
+import ThemeSwitcher from '@/components/ThemeSwitcher';
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between border-b border-border h-[60px] px-4 py-2 items-center">
+        <Logo />
+        <div className='items-center flex gap-4'>
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+      <main className="flex w-full flex-grow">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
